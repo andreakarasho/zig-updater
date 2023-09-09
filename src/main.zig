3e33e2de28d11fa5e8cc1d474dc2c81ce1b38e83
Andrea Angelini <andrea-angelini@outlook.it>
2023-09-09 17:41:29 +0200

    use my personal folder

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const zip = @import("zip");
 
-const output_path = "C:\\ziglang_test";
+const output_path = "C:\\ziglang";
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -14,11 +14,23 @@ pub fn main() !void {
     };
     defer client.deinit();
 
+    // var env = try std.process.getEnvMap(allocator);
+    // defer env.deinit();
+    // var it = env.iterator();
+    // while (it.next()) |entry| {
+    //     std.log.info("{s} = {s}", .{ entry.key_ptr.*, entry.value_ptr.* });
+    // }
+
     const uri = try std.Uri.parse("https://ziglang.org/download/index.json");
     var headers = std.http.Headers.init(allocator);
     defer headers.deinit();
 
-    var req = try client.request(std.http.Method.GET, uri, headers, .{});
+    var req = try client.request(
+        std.http.Method.GET,
+        uri,
+        headers,
+        .{},
+    );
     defer req.deinit();
 
     try req.start();
@@ -27,18 +39,40 @@ pub fn main() !void {
     var json_reader = std.json.reader(allocator, req.reader());
     defer json_reader.deinit();
 
-    const json = try std.json.parseFromTokenSource(std.json.Value, allocator, &json_reader, .{});
+    const json = try std.json.parseFromTokenSource(
+        std.json.Value,
+        allocator,
+        &json_reader,
+        .{},
+    );
     defer json.deinit();
 
     //json.value.dump();
-    const version = json.value.object.get("master").?.object.get("version").?.string;
-    const url = json.value.object.get("master").?.object.get("x86_64-windows").?.object.get("tarball").?.string;
-    const size = try std.fmt.parseInt(i32, json.value.object.get("master").?.object.get("x86_64-windows").?.object.get("size").?.string, 10);
+    const version = json.value.object.get("master").?
+        .object.get("version").?
+        .string;
+    const url = json.value.object.get("master").?
+        .object.get("x86_64-windows").?
+        .object.get("tarball").?
+        .string;
+    const size = try std.fmt.parseInt(
+        i32,
+        json.value.object.get("master").?
+            .object.get("x86_64-windows").?
+            .object.get("size").?
+            .string,
+        10,
+    );
     std.log.info("version: {s}\nurl: {s}\nsize: {}\n", .{ version, url, size });
 
     const uri_download = try std.Uri.parse(url);
 
-    var req_download = try client.request(std.http.Method.GET, uri_download, headers, .{});
+    var req_download = try client.request(
+        std.http.Method.GET,
+        uri_download,
+        headers,
+        .{},
+    );
     defer req_download.deinit();
 
     try req_download.start();
@@ -49,8 +83,15 @@ pub fn main() !void {
 
     _ = try req_download.reader().readAll(zip_buf);
 
-    if (zip.zip_stream_open(zip_buf.ptr, zip_buf.len, zip.ZIP_DEFAULT_COMPRESSION_LEVEL, 'r')) |archive| {
+    if (zip.zip_stream_open(
+        zip_buf.ptr,
+        zip_buf.len,
+        zip.ZIP_DEFAULT_COMPRESSION_LEVEL,
+        'r',
+    )) |archive| {
         defer zip.zip_stream_close(archive);
+
+        try std.fs.cwd().deleteTree(output_path);
 
         const total: usize = @intCast(zip.zip_entries_total(archive));
         std.log.info("total {}\n", .{total});
@@ -74,15 +115,17 @@ pub fn main() !void {
 
                 const res = zip.zip_entry_fread(archive, @ptrCast(@as([*c]u8, &output)));
 
-                if (res < 0) {
-                    std.log.err("error on reading: {}", .{res});
-                }
+                if (res < 0) std.log.err("error on reading: {}", .{res});
             }
         }
     }
 }
 
-fn replaceRootPath(original_path: []const u8, target_path: []const u8, output: []u8) void {
+fn replaceRootPath(
+    original_path: []const u8,
+    target_path: []const u8,
+    output: []u8,
+) void {
     @memset(output[0..], 0);
 
     var index: ?usize = null;
@@ -96,5 +139,11 @@ fn replaceRootPath(original_path: []const u8, target_path: []const u8, output: [
     if (index == null)
         index = original_path.len;
 
-    _ = std.mem.replace(u8, original_path, original_path[0..index.?], target_path, output);
+    _ = std.mem.replace(
+        u8,
+        original_path,
+        original_path[0..index.?],
+        target_path,
+        output,
+    );
 }
